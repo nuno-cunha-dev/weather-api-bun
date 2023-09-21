91dbd2e69c4b26f84120cc082276654115e65cf1
Nuno Cunha <nunorafael_6@hotmail.com>
2023-09-21 23:41:39 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import config from "./config";
 const app = new Elysia();
 app.use(cors());
 app.use(routes);
-app.listen(config.port);
+app.listen({
+  port: config.port,
+  hostname: '0.0.0.0'
+});
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
